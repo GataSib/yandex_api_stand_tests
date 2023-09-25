e4e49d3adc02a25e5d89154e19b44eba0a2ac110
Катя <kate@MacBook-Air-Ekaterina.local>
2023-09-25 21:31:25 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://193012ca-181a-41c5-bd25-e0486a8b37aa.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
